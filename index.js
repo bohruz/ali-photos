5f57fb66aedfaf07aac409bc751bc5fc4ee05016
Michael Henrique <Oliveirasantos.mh@gmail.com>
2020-09-03 15:23:11 -0300

    refactor: Refactoring the main code

--- index.js
+++ index.js
@@ -1,44 +1,30 @@
 const scrape = require("aliexpress-product-scraper");
 const download = require("image-downloader");
 
-// options = {
-//   url: "http://someurl.com/image2.jpg",
-//   dest: "/path/to/dest/photo.jpg", // will be saved to /path/to/dest/photo.jpg
-// };
+const saveImage = (url, path) => {
+  options = {
+    url: url,
+    dest: path, // will be saved to /path/to/dest/photo.jpg
+  };
 
-// download
-//   .image(options)
-//   .then(({ filename }) => {
-//     console.log("Saved to", filename); // saved to /path/to/dest/photo.jpg
-//   })
-//   .catch((err) => console.error(err));
+  download
+    .image(options)
+    .then(({ filename }) => {
+      console.log("Saved to", filename); // saved to /path/to/dest/photo.jpg
+    })
+    .catch((err) => console.error(err));
+};
 
-// const product = scrape("32958933105");
-const product = scrape("4001081693957");
+// Product ID
+const product = scrape("4000299536893");
 
-let photos = [];
 product.then((res) => {
-  //   console.log("The JSON: ", res);
+  let count = 0;
   res.feedback.forEach((feedback) => {
     feedback.photos.forEach((photo) => {
-      if (photo) photos.push(photo);
+      saveImage(photo, `./imagens/imagem - ${count++}.jpeg`);
     });
   });
-  console.log(photos);
 
-  let count = 0;
-  photos.forEach((photo) => {
-    options = {
-      //   url: "http://someurl.com/image2.jpg",
-      url: photo,
-      dest: `./imagens/imagem - ${count}.jpeg`, // will be saved to /path/to/dest/photo.jpg
-    };
-    download
-      .image(options)
-      .then(({ filename }) => {
-        console.log("Saved to", filename); // saved to /path/to/dest/photo.jpg
-      })
-      .catch((err) => console.error(err));
-    count++;
-  });
+  console.log(`${count} Images was saved`);
 });
